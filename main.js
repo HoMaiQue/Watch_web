3c1f6147b1f2f440b96240d06be601eee5a8b7c1
HoMaiQue <homaique.it@gmail.com>
2021-07-26 19:26:17 +0700

    add slider

--- main.js
+++ main.js
@@ -8,4 +8,47 @@ $(document).ready(function () {
         $('.overlay').removeClass('active');
         $('.nav').removeClass('active');
     })
+
+    $(window).scroll(() => {
+        let location = $('html').scrollTop();
+        let width = $(window).width();
+        console.log(location);
+        if (location > 200 && width > 768) {
+            $('.nav').addClass('stickNav');
+        } else {
+            $('.nav').removeClass('stickNav');
+        }
+    })
+
+    $('.swiper-button').click(() => {
+        $('.slide__information').addClass('active');
+    })
+    $('.swiper-button').mouseup(() => {
+        $('.slide__information').removeClass('active');
+    })
+
 });
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    grabCursor: true,
+    animation: {
+
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+
+});
+
+
+
+
+
+
